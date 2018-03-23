40b4d2fbc4aa8cc785e4dc7b8904b4a2209916cd
AMCorvi <am@corvi.com>
2018-03-23 13:46:06 -0400

    -Addded: comments to hammerspoon 'init' file.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,26 +1,22 @@
 local modalKey = {"cmd", "alt", "ctrl"}
 local resizeMappings = {
-  h={x=0, y=0, w=0.5, h=1},
-  j={x=0, y=0.5, w=1, h=0.5},
-  k={x=0, y=0, w=1, h=0.5},
-  l={x=0.5, y=0, w=0.5, h=1},
-  m={x=0, y=0, w=1, h=1},
-  u={x=0.5, y=0.5, w=0.5, h=0.5},
-  i={x=0, y=0.5, w=0.5, h=0.5},
-  o={x=0.5, y=0, w=0.5, h=0.5},
-  p={x=0, y=0, w=0.5, h=0.5},
+  h={x=0, y=0, w=0.5, h=1}, -- half-screen left
+  j={x=0, y=0.5, w=1, h=0.5}, -- half-screen right
+  k={x=0, y=0, w=1, h=0.5}, -- half-screen bottom
+  l={x=0.5, y=0, w=0.5, h=1}, -- half-screen top
+  m={x=0, y=0, w=1, h=1}, -- maximize screen
+  u={x=0.5, y=0.5, w=0.5, h=0.5}, -- bottom right corner
+  i={x=0, y=0.5, w=0.5, h=0.5}, -- bottom left corner
+  o={x=0.5, y=0, w=0.5, h=0.5}, -- right left corner
+  p={x=0, y=0, w=0.5, h=0.5}, -- top left corner
 
-  c={x=0.25, y=0.25, w=0.5, h=0.5},
+  c={x=0.25, y=0.25, w=0.5, h=0.5}, -- center screen
 
-  b={x=0, y=0, w=0.75, h=1},
-  n={x=0.75, y=0, w=0.25, h=1},
+  b={x=0, y=0, w=0.75, h=1}, -- 3/4 screen width
+  n={x=0.75, y=0, w=0.25, h=1}, -- 1/4 Screen width
 }
 
-hs.hotkey.bind(modalKey, "f", function()
-  local win = hs.window.focusedWindow()
-   win:setFullScreen(not win:isFullScreen())
-end)
-
+-- set screen resizing mapping
 for key in pairs(resizeMappings) do
   hs.hotkey.bind(modalKey, key, function()
     local win = hs.window.focusedWindow()
@@ -28,10 +24,20 @@ for key in pairs(resizeMappings) do
   end)
 end
 
+-- set app to full screen
+hs.hotkey.bind(modalKey, "f", function()
+  local win = hs.window.focusedWindow()
+   win:setFullScreen(not win:isFullScreen())
+end)
+
+
+
+-- refresh hammerspoon with this file(init.lua)
 hs.hotkey.bind(modalKey, "r", function()
   hs.reload()
 end)
 
+-- a number of shortcut key used to focus(or open) apps
 local focusKeys = {
 
   -- a='Atom',
@@ -45,11 +51,13 @@ local focusKeys = {
   -- v="Visual Studio Code"
 }
 
+-- set focus key to hammerspoon
 for key in pairs(focusKeys) do
   hs.hotkey.bind(modalKey, key, function()
     hs.application.launchOrFocus(focusKeys[key])
   end)
 end
+
 
 hs.hotkey.bind({"alt","shift"}, "l", function()
   send_window_next_monitor()
